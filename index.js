8d8072f0ab5f9908cd8353e1c4112334e8521a8b
robera <roberawakjira1@gmail.com>
2025-08-26 08:22:27 -0700

    docs: add comments

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const express = require('express');
+// Import required modules
+const express = require("express");        // Express framework for building APIs
+const { v4: uuidv4 } = require("uuid");    // UUID library to generate unique user IDs
+
+const app = express();
+app.use(express.json());
+// This line tells Express to automatically read JSON data
+// from the request body (what the client sends) 
+// and make it available in req.body
